ab7bea7915c2a74bccbd2cde0940e20553b2f180
morphh7 <bilguun30007688@gmail.com>
2025-04-24 14:33:26 +0200

    mycommit "index,js"

---
+++ java/index.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const header = document.querySelector("header");
+
+    window.addEventListener("scroll", () => {
+        const scrollPosition = window.scrollY;
+
+        if (scrollPosition > 30) {
+            header.classList.add("show");
+        } else {
+            header.classList.remove("show");
+        }
+    });
+});
